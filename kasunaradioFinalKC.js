d776df9ea0ed9056d961b98b1c4f62fac64c379d
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2022-04-08 02:53:19 +0200

    Update kasunaradioFinalKC.js

--- kasunaradioFinalKC.js
+++ kasunaradioFinalKC.js
@@ -165,8 +165,8 @@
                                 "" != ze && window.open(ze)
                             }).kasunaradiodisableSelection(), (i = document.createElement("div")).id = n + "coverwrapper1", a.appendChild(i), e("#" + n + "coverwrapper1").css({
                                 position: "absolute",
-                                left: "25px",
-                                top: "55px",
+                                left: "15px",
+                                top: "35px",
                                 height: "80px",
                                 width: "80px",
                                 overflow: "hidden",
@@ -176,8 +176,8 @@
                                 "background-size": "cover"
                             }), (i = document.createElement("div")).id = n + "coverwrapper2", a.appendChild(i), e("#" + n + "coverwrapper2").css({
                                 position: "absolute",
-                                left: "25px",
-                                top: "55px",
+                                left: "15px",
+                                top: "35px",
                                 height: "80px",
                                 width: "80px",
                                 overflow: "hidden",
