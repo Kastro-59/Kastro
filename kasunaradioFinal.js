78eb61b73a88b1417f98d5ede02932369670df83
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2022-11-22 18:55:41 +0100

    Update kasunaradioFinal.js

--- kasunaradioFinal.js
+++ kasunaradioFinal.js
@@ -769,7 +769,7 @@
 							
 case "radiozeno":
 ! function() {
-var t = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/" + zn +"?_=" + Math.random();
+var t = "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/" + zn +"?rand=" + Math.random();
 e.ajax({
 dataType: "text",
 method: "GET",
