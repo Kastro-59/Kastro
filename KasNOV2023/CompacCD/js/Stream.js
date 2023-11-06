70014836e0206a921afe22c1ea86366dc9e7b939
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-11-06 22:19:16 +0100

    Add files via upload

---
+++ KasNOV2023/CompacCD/js/Stream.js
@@ -0,0 +1,9 @@
+ $("#stream1").CompacCD({ 
+ URL: "https://jm8n.net:7020", 
+version: "2", 
+artwork: true, 
+show_listeners: true, 
+logo: "", 
+servertitle: "Radio-DINARAMA", 
+autoplay: false 
+}) 
